06d660859c7911d6389d2656fd27399a154edbce
Sheika <sheika02x2@gmail.com>
2026-06-22 16:30:46 +0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
