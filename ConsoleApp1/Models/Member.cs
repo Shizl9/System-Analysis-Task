d7782f331cf015a98b3cf32b9de5b1b0d4a7644d
Sheika <sheika02x2@gmail.com>
2026-06-23 08:50:08 +0400

    adding class member and identify how each proberty gets its value

---
+++ ConsoleApp1/Models/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Models
+{
+    public class Member
+    {
+        public int memberId { get; set; } //system generated
+        public string memberName { get; set; } // input
+        public string memberPhone { get; set; } //input
+        public string memberEmail { get; set; } //input
+        public string registrationDate { get; set; } //defult value
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,10 +2,7 @@
 {
     internal class Program
     {
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="args"></param>
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
